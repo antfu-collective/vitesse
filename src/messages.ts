540a51e3dd07c8998a129926911cb88e82da05e7
buzz-js <63687573+buzz-js@users.noreply.github.com>
2020-08-20 17:57:39 +0200

    Update messages.ts

--- src/messages.ts
+++ src/messages.ts
@@ -1,11 +1,13 @@
 import en from '../locales/en.json'
 import zhCN from '../locales/zh-CN.json'
 import fr from '../locales/fr.json'
+import es from '../locales/es.json'
 
 export const messages = {
   en,
   'zh-CN': zhCN,
   fr,
+  es,
 }
 
 export const locales = Object.keys(messages)

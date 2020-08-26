7fa1b1556a2537300be3018d48371d7a25f42406
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-26 11:15:08 +0800

    fix: vite-plugin-components alias resolving, close #10

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import Voie from 'vite-plugin-voie'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import ViteComponents from 'vite-plugin-components'
 
+const alias = {
+  '/~/': path.resolve(__dirname, 'src'),
+}
+
 const config: UserConfig = {
-  alias: {
-    '/~/': path.resolve(__dirname, 'src'),
-  },
+  alias,
   plugins: [
     Voie(),
-    ViteComponents(),
+    ViteComponents({
+      // currently, vite does not provide an API for plugins to get the config https://github.com/vitejs/vite/issues/738
+      // as the `alias` changes the behavior of middlewares, you have to pass it to ViteComponents to do the resolving
+      alias,
+    }),
     PurgeIcons(),
   ],
 }

831cc305684b763346a81a7a0dd8b6f694affcdb
Harry Trần <nhattq.coding@gmail.com>
2020-11-30 14:01:24 +0700

    i18n(vi): add vietnamese translation (#17)

--- src/messages.ts
+++ src/messages.ts
@@ -2,12 +2,14 @@ import en from '../locales/en.json'
 import zhCN from '../locales/zh-CN.json'
 import fr from '../locales/fr.json'
 import es from '../locales/es.json'
+import vi from '../locales/vi.json'
 
 export const messages = {
   en,
   'zh-CN': zhCN,
   fr,
   es,
+  vi,
 }
 
 export const locales = Object.keys(messages)

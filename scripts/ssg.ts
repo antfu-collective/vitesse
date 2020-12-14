9f553c0207807b1cc1e78468895739a68cc6bb63
Anthony Fu <anthonyfu117@hotmail.com>
2020-12-14 15:34:42 +0800

    chore: ci

--- scripts/ssg.ts
+++ scripts/ssg.ts
@@ -2,4 +2,6 @@ import { execSync } from 'child_process'
 // @ts-ignore
 import { path } from 'chromium'
 
+console.log('Starting SSG...')
 execSync(`npx cross-env CHROME_PATH="${path}" presite dist`, { stdio: 'inherit' })
+console.log('SSG Finished...')

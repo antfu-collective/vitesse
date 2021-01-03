800804c08d302d40a0b1a33ec55c6bf555af4ecb
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-04 02:21:10 +0800

    wip: vite 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { UserConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
 import Voie from 'vite-plugin-voie'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import ViteComponents from 'vite-plugin-components'
@@ -7,13 +8,13 @@ import Markdown from 'vite-plugin-md'
 import Shiki from 'markdown-it-shiki'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const alias = {
-  '/~/': path.resolve(__dirname, 'src'),
-}
-
 const config: UserConfig = {
-  alias,
+  alias: [
+    { find: /\/~\//g, replacement: `${path.resolve(__dirname, 'src')}/` },
+  ],
   plugins: [
+    Vue(),
+
     // https://github.com/vamplate/vite-plugin-voie
     Voie({
       // load index page sync and bundled with the landing page to improve first loading time.
@@ -43,15 +44,11 @@ const config: UserConfig = {
     ViteComponents({
       // currently, vite does not provide an API for plugins to get the config https://github.com/vitejs/vite/issues/738
       // as the `alias` changes the behavior of middlewares, you have to pass it to ViteComponents to do the resolving
-      alias,
-
       // allow auto load markdown components under `./src/components/`
       extensions: ['vue', 'md'],
 
       // allow auto import and register components used in markdown
-      customLoaderMatcher({ path }) {
-        return path.endsWith('.md')
-      },
+      customLoaderMatcher: id => id.endsWith('.md'),
 
       // auto import icons
       customComponentResolvers: [

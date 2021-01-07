72fedb9b8f6b32534ca0602ab452c2d9fa1b2151
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-07 13:47:54 +0800

    feat: vite 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import Voie from 'vite-plugin-voie'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import ViteComponents from 'vite-plugin-components'
 import Markdown from 'vite-plugin-md'
-import Shiki from 'markdown-it-shiki'
+import Prism from 'markdown-it-prism'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const config: UserConfig = {
@@ -32,13 +32,8 @@ const config: UserConfig = {
       // for https://github.com/tailwindlabs/tailwindcss-typography
       wrapperClasses: 'prose prose-sm m-auto',
       markdownItSetup(md) {
-        // https://github.com/antfu/markdown-it-shiki
-        md.use(Shiki, {
-          theme: {
-            dark: 'min-dark',
-            light: 'min-light',
-          },
-        })
+        // https://prismjs.com/
+        md.use(Prism)
       },
     }),
 
@@ -66,25 +61,25 @@ const config: UserConfig = {
     ViteIcons(),
 
     // https://github.com/antfu/vite-plugin-pwa
-    // VitePWA({
-    //   manifest: {
-    //     name: 'Vitesse',
-    //     short_name: 'Vitesse',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: '/pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      manifest: {
+        name: 'Vitesse',
+        short_name: 'Vitesse',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
 }
 

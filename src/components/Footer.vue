d687faefac43f3c8c1659e1c367c8d398f07c333
kazuya kawaguchi <kawakazu80@gmail.com>
2021-01-09 16:47:03 +0900

    refactor: move resource importing to i18n.ts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,13 +25,13 @@
 
 <script setup lang='ts'>
 import { useI18n } from 'vue-i18n'
-import { locales } from '/~/messages'
 import { isDark } from '/~/logics'
 
 const i18n = useI18n()
 
 const toggleLocales = () => {
   // change to some real logic
+  const locales = i18n.availableLocales
   i18n.locale.value = locales[(locales.indexOf(i18n.locale.value) + 1) % locales.length]
 }
 

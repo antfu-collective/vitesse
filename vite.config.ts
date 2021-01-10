411755ea476b280b9fd228c42683f0bce5dd8857
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-10 17:13:02 +0800

    chore: clean up comments

--- vite.config.ts
+++ vite.config.ts
@@ -40,8 +40,6 @@ const config: UserConfig = {
 
     // https://github.com/antfu/vite-plugin-components
     ViteComponents({
-      // currently, vite does not provide an API for plugins to get the config https://github.com/vitejs/vite/issues/738
-      // as the `alias` changes the behavior of middlewares, you have to pass it to ViteComponents to do the resolving
       // allow auto load markdown components under `./src/components/`
       extensions: ['vue', 'md'],
 

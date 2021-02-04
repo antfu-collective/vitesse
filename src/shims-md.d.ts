7ddf0869750d2ac1b257201807ed06a5b17d9e7b
HomyeeKing <HomyeeKing@gmail.com>
2021-02-04 18:47:57 +0800

    disable shim lint

--- src/shims-md.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.md' {
-  import { ComponentOptions } from 'vue'
-  const Component: ReturnType<typeof defineComponent>
-  export default Component
-}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,11 @@
+/* eslint-disable import/no-duplicates */
 declare module '*.vue' {
-  import { ComponentOptions } from 'vue'
+  import { ComponentOptions, ComponentOptions } from 'vue'
   const component: ReturnType<ComponentOptions>
   export default component
+}
+declare module '*.md' {
+  import { ComponentOptions } from 'vue'
+  const Component: ReturnType<typeof defineComponent>
+  export default Component
 }

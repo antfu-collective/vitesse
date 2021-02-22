1207a86716ae5e4392ee420fe91b02619fca1fd4
Nandor Dudas <nandor.dudas@gmail.com>
2021-02-22 22:10:56 +0100

    refactor: color scheme load

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { tryOnMounted } from '@vueuse/core'
 import { useHead } from '@vueuse/head'
+import { nextTick } from 'vue'
 
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
@@ -9,6 +11,16 @@ useHead({
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
   ],
+})
+
+tryOnMounted(async() => {
+  await nextTick()
+
+  const { matches: prefersDark } = window.matchMedia('(prefers-color-scheme: dark)')
+  const setting = localStorage.getItem('color-schema') ?? 'auto'
+
+  if (setting === 'dark' || (prefersDark && setting !== 'light'))
+    document.documentElement.classList.toggle('dark', true)
 })
 </script>
 

8c1c8aff912d4477189d663247b0966d6f0c2875
Andrea Nanni <andrea.nanni3@gmail.com>
2021-02-28 22:09:46 +0100

    update vite-plugin-vue-i18n, remove unnecessary glob import

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,15 +1,6 @@
 import { createI18n } from 'vue-i18n'
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
 import { UserModule } from '~/types'
-
-// import i18n resources
-// https://vitejs.dev/guide/features.html#glob-import
-const messages = Object.fromEntries(
-  Object.entries(
-    import.meta.globEager('../../locales/*.json'))
-    .map(([key, value]) => {
-      return [key.slice(14, -5), value.default]
-    }),
-)
 
 export const install: UserModule = ({ app }) => {
   const i18n = createI18n({

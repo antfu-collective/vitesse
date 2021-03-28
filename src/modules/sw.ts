066984a8fc8b9f89d4f1548eaa1dc10b8097f80b
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-03-28 14:55:17 +0200

    chore: clean up

--- src/modules/sw.ts
+++ src/modules/sw.ts
@@ -3,10 +3,8 @@ import { UserModule } from '~/types'
 export const install: UserModule = ({ isClient, router }) => {
   if (isClient) {
     router.isReady().then(async() => {
-      if (isClient) {
-        const { registerSW } = await import('virtual:pwa-register')
-        registerSW({ immediate: true })
-      }
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({ immediate: true })
     })
   }
 }

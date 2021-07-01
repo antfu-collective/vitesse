b44925c9584cb19d57923dfd4e12307bfc8b905a
Oliver Weiß <oliver.weiss@appsfactory.de>
2021-07-01 14:25:32 +0200

    add mising .vue shim for ts

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -10,3 +10,9 @@ declare module '*.md' {
   const component: ComponentOptions
   export default component
 }
+
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const component: ComponentOptions
+  export default component
+}

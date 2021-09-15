3f9e5f93ebdf7e1c14691f360b9562d570f5f077
Manuel Quarneti <manuelquarneti@gmail.com>
2021-09-15 13:42:03 +0200

    chore: useHead is auto-imported (#192)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useHead } from '@vueuse/head'
-
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg

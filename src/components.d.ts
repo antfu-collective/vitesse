d8b2512381470f46505d20755202dfd90cb7dcae
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-30 05:16:50 +0800

    chore: update deps

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
     CarbonLanguage: typeof import('~icons/carbon/language')['default']
     CarbonLogoGithub: typeof import('~icons/carbon/logo-github')['default']
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
+    CarbonPedestrian: typeof import('~icons/carbon/pedestrian')['default']
     CarbonSun: typeof import('~icons/carbon/sun')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']

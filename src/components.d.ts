e4617a0076d03d8544fcc9c8cac0a151263b3de7
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-30 05:17:14 +0800

    chore: update dts file

--- src/components.d.ts
+++ src/components.d.ts
@@ -11,6 +11,7 @@ declare module 'vue' {
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
     CarbonPedestrian: typeof import('~icons/carbon/pedestrian')['default']
     CarbonSun: typeof import('~icons/carbon/sun')['default']
+    CarbonWarning: typeof import('~icons/carbon/warning')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
   }

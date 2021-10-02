27ee5ecddf09ec7fd8d2bf1954d7a02660bb80bb
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-10-02 13:17:18 -0400

    added concurrently, bumped deps

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Loving Memory',
+  title: 'Vite/Vue Tauri Application',
   meta: [
-    { name: 'description', content: 'Memorial Site' },
+    { name: 'description', content: 'Vite/Vue Tauri Application' },
   ],
 })
 </script>

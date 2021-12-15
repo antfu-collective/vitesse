abf9100ad9291301f2b433191aae828787f576cb
YunYouJun <me@yunyoujun.cn>
2021-12-16 00:40:03 +0800

    chore: add type for import by eslint

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,5 +1,5 @@
 import { createI18n } from 'vue-i18n'
-import { UserModule } from '~/types'
+import type { UserModule } from '~/types'
 
 // Import i18n resources
 // https://vitejs.dev/guide/features.html#glob-import
--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -1,5 +1,5 @@
 import { createPinia } from 'pinia'
-import { UserModule } from '~/types'
+import type { UserModule } from '~/types'
 
 // Setup Pinia
 // https://pinia.esm.dev/

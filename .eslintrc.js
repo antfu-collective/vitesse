dbff4abccb9c4394c9d0079ab17f4b11b36a4c58
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-01-27 12:35:50 +0800

    fix: eslint config for .js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,38 @@
-module.exports = {
-  root: true,
+const tsConfig = {
   extends: '@daotl/eslint-config-vue/typescript',
   parserOptions: {
     project: 'tsconfig.json',
   },
+}
+
+module.exports = {
+  root: true,
   rules: {
+    'no-undef': 'off', // Turned off for `unplugin-auto-import`
     'import/no-unresolved': ['error', { ignore: ['^virtual:'] }],
+    'vue/multi-word-component-names': 'off',
   },
+  overrides: [
+    // For TS
+    {
+      files: '**/*.{ts,tsx,vue}',
+      ...tsConfig,
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+    // For JS and config files (.fes.js .eslintrc.js .stylelintrc.js)
+    {
+      files: '**/*.{js,jsx}',
+      extends: '@daotl/eslint-config-vue',
+    },
+    // For tests
+    {
+      files: ['**/*.spec.{j,t}s?(x)'],
+      ...tsConfig,
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }

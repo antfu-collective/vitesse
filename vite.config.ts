ebee2d81f9ad4836b2d13df6c47337e5ebf4c21a
JB Aubrée <jb.aubree@digitsole.com>
2022-02-22 23:55:39 +0100

    fix: remove sitemap data from VitePWA includeAssets

--- vite.config.ts
+++ vite.config.ts
@@ -88,7 +88,7 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'sitemap.xml', 'robots.txt', 'safari-pinned-tab.svg'],
+      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
         name: 'Vitesse',
         short_name: 'Vitesse',

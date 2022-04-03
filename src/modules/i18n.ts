284b0075977a66c2c4cbace392cf9be5c8137470
YunYouJun <me@yunyoujun.cn>
2022-04-04 01:30:59 +0800

    refactor: use static bundle importing for i18n

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,18 +1,13 @@
 import { createI18n } from 'vue-i18n'
-import type { UserModule } from '~/types'
-
 // Import i18n resources
-// https://vitejs.dev/guide/features.html#glob-import
-//
 // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
-const messages = Object.fromEntries(
-  Object.entries(
-    import.meta.globEager('../../locales/*.y(a)?ml'))
-    .map(([key, value]) => {
-      const yaml = key.endsWith('.yaml')
-      return [key.slice(14, yaml ? -5 : -4), value.default]
-    }),
-)
+/*
+ * Static bundle importing https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n#static-bundle-importing
+ * All i18n resources specified in the plugin `include` option can be loaded
+ * at once using the import syntax
+ */
+import messages from '@intlify/vite-plugin-vue-i18n/messages'
+import type { UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
   const i18n = createI18n({

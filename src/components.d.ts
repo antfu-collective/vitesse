d55eaca73164d172f09d2485e04f34b32956df4e
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-10 10:28:44 +0800

    chore: update deps

--- src/components.d.ts
+++ src/components.d.ts
@@ -2,11 +2,13 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     README: typeof import('./components/README.md')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
   }
 }
 

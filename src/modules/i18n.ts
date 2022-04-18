297e102b852e2a133e0e54a17c8c1b3c2937cb96
ntnyq <ntnyq@foxmail.com>
2022-04-18 22:41:36 +0800

    chore: tweak

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -8,10 +8,7 @@ import { type UserModule } from '~/types'
 const messages = Object.fromEntries(
   Object.entries(
     import.meta.globEager('../../locales/*.y(a)?ml'))
-    .map(([key, value]) => {
-      const yaml = key.endsWith('.yaml')
-      return [key.slice(14, yaml ? -5 : -4), value.default]
-    }),
+    .map(([key, value]) => [key.split('/').at(-1)?.replace(/\.ya?ml$/, ''), value.default]),
 )
 
 export const install: UserModule = ({ app }) => {

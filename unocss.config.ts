225d10511e91392fffd5629ecbc00d6722c69ded
ntnyq <ntnyq13@gmail.com>
2022-04-18 23:27:36 +0800

    chore: tweak (#333)

--- unocss.config.ts
+++ unocss.config.ts
@@ -12,7 +12,7 @@ import {
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
   ],
   presets: [
     presetUno(),

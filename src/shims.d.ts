23edd065d708df3fa143fdb444fcbdcca78e9a16
Chris <chrisbreuer93@gmail.com>
2022-04-26 09:45:51 -0700

    chore: adjust the comment

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -2,7 +2,7 @@ declare interface Window {
   // extend the window
 }
 
-// with vite-plugin-md, markdowns can be treat as Vue components
+// with vite-plugin-md, markdown files can be treated as Vue components
 declare module '*.md' {
   import { type DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>

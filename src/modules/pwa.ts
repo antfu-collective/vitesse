02650225182c946f631103624da597271cd488d4
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-01 11:52:59 +0800

    chore: lint

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -5,7 +5,7 @@ export const install: UserModule = ({ isClient, router }) => {
   if (!isClient)
     return
 
-  router.isReady().then(async() => {
+  router.isReady().then(async () => {
     const { registerSW } = await import('virtual:pwa-register')
     registerSW({ immediate: true })
   })
--- test/component.test.ts
+++ test/component.test.ts
@@ -9,7 +9,7 @@ describe('Counter.vue', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  it('should be interactive', async() => {
+  it('should be interactive', async () => {
     const wrapper = mount(Counter, { props: { initial: 0 } })
     expect(wrapper.text()).toContain('0')
 

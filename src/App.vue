21b800509be3e3030d8620fdb64a809522f5de12
云游君 <me@yunyoujun.cn>
2022-05-07 05:09:44 +0800

    feat: toggle meta theme-color with mode (#348)
    * feat: toggle meta theme-color with mode
    
    * chore: fix lint

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+import { isDark } from '~/composables'
+
 useHead({
   title: 'Vitesse',
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
   ],
 })
 </script>

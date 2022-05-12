fc482c3f56ddc74486afec2ac325a1f85ebcd1bc
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-05-12 20:36:41 +0800

    style: shims.d.ts

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -6,7 +6,7 @@
 
 // with vite-plugin-md, markdowns can be treat as Vue components
 declare module '*.md' {
-  import type { DefineComponent } from 'vue'
+  import { type DefineComponent } from 'vue'
 
   const component: DefineComponent<
     Record<string, unknown>,

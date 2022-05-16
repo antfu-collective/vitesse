2c29e41d523e78a3c56a8ac944c56b5df7d4d708
ハン / Han <me@hanlee.co>
2022-05-16 12:07:37 +0900

    fix: favicon dark mode (#359)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-import { isDark } from '~/composables'
+import { isDark, preferredDark } from '~/composables'
 
 useHead({
   title: 'Vitesse',
@@ -11,6 +11,13 @@ useHead({
     {
       name: 'theme-color',
       content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
     },
   ],
 })

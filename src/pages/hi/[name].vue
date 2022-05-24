4f220a63b48faaa5988a7d3b1371bd3ddd693441
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-24 15:17:55 +0800

    feat: enable composable auto import

--- src/pages/hi/[name].vue
+++ src/pages/hi/[name].vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useUserStore } from '~/stores/user'
-
 const props = defineProps<{ name: string }>()
 const router = useRouter()
 const user = useUserStore()
--- vite.config.ts
+++ vite.config.ts
@@ -48,6 +48,11 @@ export default defineConfig({
         '@vueuse/core',
       ],
       dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/store',
+      ],
+      vueTemplate: true
     }),
 
     // https://github.com/antfu/unplugin-vue-components

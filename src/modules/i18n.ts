43ea74990cc8c258fb9b85d62cb362f82aff27e9
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-27 12:12:39 +0200

    chore: update

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -12,32 +12,36 @@ const i18n = createI18n({
   messages: {},
 })
 
-export const availableLocales = (import.meta.env.locales).split(',')
+const localesMap = Object.fromEntries(
+  Object.entries(import.meta.glob('../../locales/*.yml'))
+    .map(([path, loadLocale]) => [path.match(/([\w-]*)\.yml$/)?.[1], loadLocale]),
+) as Record<Locale, () => Promise<{ default: Record<string, string> }>>
+
+export const availableLocales = Object.keys(localesMap)
+
 const loadedLanguages: string[] = []
 
 function setI18nLanguage(lang: Locale) {
   i18n.global.locale.value = lang as any
-  document.querySelector('html')?.setAttribute('lang', lang)
+  if (typeof document !== 'undefined')
+    document.querySelector('html')?.setAttribute('lang', lang)
   return lang
 }
 
-export function loadLanguageAsync(lang: string) {
+export async function loadLanguageAsync(lang: string): Promise<Locale> {
   // If the same language
   if (i18n.global.locale.value === lang)
-    return Promise.resolve(setI18nLanguage(lang))
+    return setI18nLanguage(lang)
 
   // If the language was already loaded
   if (loadedLanguages.includes(lang))
-    return Promise.resolve(setI18nLanguage(lang))
+    return setI18nLanguage(lang)
 
   // If the language hasn't been loaded yet
-  return import(`../../locales/${lang}.yml`).then(
-    (messages) => {
-      i18n.global.setLocaleMessage(lang, messages.default)
-      loadedLanguages.push(lang)
-      return setI18nLanguage(lang)
-    },
-  )
+  const messages = await localesMap[lang]()
+  i18n.global.setLocaleMessage(lang, messages.default)
+  loadedLanguages.push(lang)
+  return setI18nLanguage(lang)
 }
 
 export const install: UserModule = ({ app }) => {

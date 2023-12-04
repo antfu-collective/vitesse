924e913ce9fdb81a557895866269ea05957d8fcc
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-04 18:03:25 +0100

    chore: enable also toml formatting

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,6 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     unocss: true,
-    formatters: {
-      html: true,
-      css: true,
-      markdown: true,
-    },
+    formatters: true,
   },
 )

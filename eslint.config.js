1f66f2f664565f5c7e38084a122d71b29d19e782
Nex Zhu <hitnexup@gmail.com>
2023-12-18 15:08:42 +0800

    setup: add nx

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,21 @@ import config from '@daotl/eslint-config'
 export default config({
   unocss: true,
 }, {
-  ignores: ['cypress'],
-})
+  ignores: ['cypress', '.nx', 'nx.json', '**/*.md', 'tsconfig.*'],
+},
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.build.json'],
+      }
+    }
+  },
+  {
+    files: ["**/*.spec.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.spec.json'],
+      }
+    }
+  },
+)
---
+++ nx.js
@@ -0,0 +1,120 @@
+/* eslint-disable no-template-curly-in-string */
+
+import fs from 'node:fs'
+
+import * as R from 'remeda'
+
+// Default: ['{projectRoot}/dist', '{projectRoot}/build']
+const outputs = []
+
+const inputsIgnore = R.map(R.map(outputs, R.concat), fn => fn('!'))
+
+const config = {
+  $schema: './node_modules/nx/schemas/nx-schema.json',
+  namedInputs: {
+    sharedGlobals: [
+      '{projectRoot}/.env.*',
+      '{projectRoot}/tsconfig.json',
+      { env: 'NODE_ENV' },
+    ],
+    default: [
+      'sharedGlobals',
+      '{projectRoot}/**/*',
+      '!{projectRoot}/**/?(*.)+spec.ts',
+    ],
+  },
+  targetDefaults: {
+    build: {
+      dependsOn: ['^build'],
+      cache: true,
+      executor: '@nx/vite:tsc',
+      inputs: ['default'],
+      outputs: ['{options.outputPath}'],
+      options: {
+        clean: false,
+      },
+      configurations: {
+        ci: {
+          options: {
+            // Generate a lockfile (e.g. package-lock.json) that matches the workspace lockfile to ensure package versions match.
+            generateLockfile: true,
+          },
+        },
+      },
+    },
+
+    test: {
+      dependsOn: ['build'],
+      cache: true,
+      executor: '@nx/vite:test',
+      outputs: ['{options.reportsDirectory}'],
+      inputs: [
+        '{projectRoot}/src/**/*.js',
+        '{projectRoot}/src/**/*.jsx',
+        '{projectRoot}/src/**/*.ts',
+        '{projectRoot}/src/**/*.tsx',
+        '{projectRoot}/test/**/*.js',
+        '{projectRoot}/test/**/*.jsx',
+        '{projectRoot}/test/**/*.ts',
+        '{projectRoot}/test/**/*.tsx',
+        '{projectRoot}/src/**/*.vue',
+      ],
+    },
+
+    dev: {
+      cache: false,
+    },
+
+    lint: {
+      dependsOn: [
+        // To pass "--fix"
+        { project: 'self', target: 'eslint', params: 'forward' },
+        { project: 'self', target: 'stylelint', params: 'forward' },
+      ],
+      cache: true,
+      inputs: [...inputsIgnore],
+      // Can't use "nx:noop" for it doesn't allow passing args
+      // executor: 'nx:noop',
+      executor: 'nx:run-commands',
+      options: {
+        command: 'echo lint done, args:',
+      },
+    },
+    eslint: {
+      cache: true,
+      executor: 'nx:run-commands',
+      options: {
+        command: 'FIX={args.fix}; eslint src --fix=${FIX:-false}',
+      },
+    },
+    stylelint: {
+      cache: true,
+      executor: 'nx:run-commands',
+      options: {
+        command: 'FIX={args.fix}; stylelint "src/**/*.{css,scss,vue,tsx}" --fix=${FIX:-false}',
+      },
+    },
+
+    container: {
+      dependsOn: ['build'],
+      cache: true,
+      options: {
+        load: true,
+        platforms: ['linux/amd64', 'linux/arm64'],
+      },
+      configurations: {
+        ci: {
+          load: false,
+          push: true,
+        },
+      },
+    },
+
+    deploy: {
+      dependsOn: ['build', 'test', 'lint'],
+      cache: false,
+    },
+  },
+}
+
+fs.writeFileSync('./nx.json', JSON.stringify(config, undefined, 2))

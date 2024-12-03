b15eaad009203cd18614c09013433b7a470a316e
Anthony Fu <github@antfu.me>
2024-12-03 14:00:28 +0800

    chore: apply lint

--- src/modules/nprogress.ts
+++ src/modules/nprogress.ts
@@ -1,5 +1,5 @@
-import NProgress from 'nprogress'
 import type { UserModule } from '~/types'
+import NProgress from 'nprogress'
 
 export const install: UserModule = ({ isClient, router }) => {
   if (isClient) {
